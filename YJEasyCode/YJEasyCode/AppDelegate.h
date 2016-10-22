d2eb456b010d112c7fcaf8ce61b48341df6525ef
YJHou <yjhou@YJHouMacBookPro.local>
2016-10-22 23:19:54 +0800

    22

---
+++ YJEasyCode/YJEasyCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJEasyCode
+//
+//  Created by YJHou on 2016/10/22.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
